52328532211579ec54a244bcf39c2694b2cd6557
aidanliddiard <aidanliddiard@gmail.com>
2022-02-01 16:21:07 -0800

    adding country-data.js

---
+++ country-data.js
@@ -0,0 +1,38 @@
+export const countries = [
+    {
+        name: 'Australia',
+        img: 'icons8-australia-96.png',
+        facts: {
+            population: '25.69 million',
+            capital: 'Canberra',
+            fact: 'If you were to visit one new beach in Australia every day, it would take more than 27 years to see them all.',
+        },
+    },
+    {
+        name: 'Botswana',
+        img: 'icons8-botswana-96.png',
+        facts: {
+            population: '2.35 million',
+            capital: 'Gaborone',
+            fact: 'Botswana shares the shortest border in the world with Zambia. It is 150 meters long.',
+        },
+    },
+    {
+        name: 'Mexico',
+        img: 'icons8-mexico-96.png',
+        facts: {
+            population: '128.9 million',
+            capital: 'Mexico City',
+            fact: 'The meteorite that caused the extinction of the dinosaurs landed in Mexico.',
+        },
+    },
+    {
+        name: 'Thailand',
+        img: 'icons8-thailand-96.png',
+        facts: {
+            population: '69.8 million',
+            capital: 'Bangkok',
+            fact: 'Thailand was the only Southeast Asian country not colonized by a European country.',
+        },
+    },
+];
